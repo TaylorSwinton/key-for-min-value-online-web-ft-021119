6e42d43b4658da8812c3b6425ed45482552df4e3
Taylor Swinton <taylorswinton1@gmail.com>
2019-02-15 01:27:24 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   lowest_key = nil
-  lowest_value = Float:: INFINITY
+  lowest_value = 600
   name_hash.each do |key, value|
     if value < lowest_value
       lowest_value = value
